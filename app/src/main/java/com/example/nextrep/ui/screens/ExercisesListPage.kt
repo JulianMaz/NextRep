a38da39996bfd6857ca7d12b15c74133b95e0a4e
JulianMaz <julian.mazerat@gmail.com>
2025-11-24 18:00:11 -0500

    Ajout d'un bouton flottant pour l'ajout d'exercices

--- app/src/main/java/com/example/nextrep/ui/screens/ExercisesListPage.kt
+++ app/src/main/java/com/example/nextrep/ui/screens/ExercisesListPage.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -57,6 +61,11 @@ fun ExercisesListPage(
                 )
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddExercise) {
+                Icon(Icons.Default.Add, contentDescription = "Add Session")
+            }
+        }
     ) { innerPadding ->
         ExerciseListContent(
             exercises = uiState.exercises,
@@ -69,12 +78,10 @@ fun ExercisesListPage(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExerciseListContent(
-    exercisesViewModel: ExercisesViewModel = viewModel(),
     exercises: List<Exercise>,
     onExerciseClick: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val uiState by exercisesViewModel.uiState.collectAsState()
 
     LazyColumn(
         modifier = modifier.fillMaxSize(),
